96c992ef171cfe50a50a1bc2670edd806dd1912b
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-16 14:41:37 +0000

    js

--- 02-Homework/Assets/script.js
+++ 02-Homework/Assets/script.js
@@ -1,68 +1,93 @@
-// Assignment Code
-var generateBtn = document.querySelector('#generate');
+//Declaring Arrays
+const lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "w",
+  "y",
+  "z",
+];
 
-function generatePassword() {
+const uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "X",
+  "W",
+  "Y",
+  "Z",
+];
 
-  const lowercase = "abcdefghijklmnopqrstuvwxyxz";
-  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = "0123456789";
-  const specials = "!@#$%^&*()?/;:~";
+const numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-
-  const passwordLength = Number(prompt("Enter password length (10-132):"));
-
-  if (passwordLength < 10 || passwordLength > 132) {
-    alert("Please choose a different length?");
-    return passwordLength;
-  } else (passwordLength >= 10 || passwordLength <= 132) {
-  }
-
-
-  const isLowerCase = confirm("Do you want to add a lowercase?");
-  const isUpperCase = confirm("Do you want to add a uppercase?");
-  const isNumbers = confirm("Do you want to add a number?");
-  const isSpecials = confirm("Do you want to add a special character?");
-
-  if (!isLowerCase && !isUpperCase && !isNumbers && !isSpecials) {
-    alert("please choose at least one option");
-    return;
-  }
-  const choices = [];
-
-  if (isLowerCase) {
-    choices.push(lowercase);
-  }
-  if (isUpperCase) {
-    choices.push(uppercase);
-  }
-  if (isNumbers) {
-    choices.push(numbers);
-  }
-  if (isSpecials) {
-    choices.push(specials);
-  }
-
-  let password = "";
-  // generate random password
-  for (let i = 0; i < 10; i++) {
-    // get random choice index
-    const randomChoiceIndex = Math.floor(Math.random() * choices.length);
-    // get random characters
-    const randomCharacters = choices[randomChoiceIndex];
-    password += randomCharacters.charAt(
-      Math.floor(Math.random() * randomCharacters.length)
-    );
-  }
-  return password;
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+const specialCharacters = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "]",
+  "[",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
